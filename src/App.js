9f7404ed7bd0ed8cf2564abd54309863e2f8b20f
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-07-30 12:32:26 +0100

    improve efficiency

--- src/App.js
+++ src/App.js
@@ -15,8 +15,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+        {this.state.modalIsOpen ? (
+          <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
+        ) : null}
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
